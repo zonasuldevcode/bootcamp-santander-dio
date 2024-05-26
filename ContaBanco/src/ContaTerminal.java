360e917768253d5ebd977b41228ce7f15e1e96b4
Leonardo Ernesto Salvador <leosalvador21@hotmail.com>
2024-05-25 21:09:03 -0300

    Criando a classe ContaTerminal

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+    public int numero;
+    public String agencia;
+    public String nomeCliente;
+    public double saldo;
+
+
+    public static void main (String[] args) throws Exception {
+       
+
+
+
+    }
+
+
+
+}
